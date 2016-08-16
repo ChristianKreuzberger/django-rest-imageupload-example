3f764cc8e3745b35c7b13859ff7a3695ba8e5b04
Christian Kreuzberger <s4ko88@gmail.com>
2016-08-16 21:08:28 +0200

    removed a non necessary line of code in urls.py

--- django_rest_imageupload_backend/imageupload_rest/urls.py
+++ django_rest_imageupload_backend/imageupload_rest/urls.py
@@ -6,7 +6,6 @@ router = routers.DefaultRouter()
 router.register('images', viewsets.UploadedImagesViewSet, 'images')
 
 # Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     url(r'^', include(router.urls)),
 ]
